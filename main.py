fab14b5adf90ded5eb79bbe4b89225bbda082a74
El Libertad Zapata <c0090945@bangkit.academy>
2021-05-11 00:39:27 +0000

    Initialize Flask, connector to database, and testing
    Flask is initialized to the App Engine. I add the connector to the VM database in the Cloud. I also make the app can be tested by running locally in the Cloud Shell.

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import mysql.connector
 from flask import Flask
 import json
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+cnx = mysql.connector.connect(user='root',
+                                password='jaki1234',
+                                host='35.219.48.119',
+                                database='jaki',
+                                auth_plugin='mysql_native_password')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True) # This is just for testing in the Cloud Shell
